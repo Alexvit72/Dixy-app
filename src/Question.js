b8bf6c7130fe5b589c2e1e7ff5498ff8599057e7
Alexvit72 <aleksandrvitebsk@tut.by>
2020-11-24 23:55:02 +0300

    fix indent #2

--- src/Question.js
+++ src/Question.js
@@ -3,17 +3,17 @@ import './css_Comp/Question.css';
 
 function Question(props) {
   let list = props.question.answers.map( (item, index) => {
-		return <label key={index}>
-			<input type='radio'
+    return <label key={index}>
+      <input type='radio'
         name='answer'
         value={'value' + index}
         tabIndex='0'
         checked={props.value === 'value' + index}
         onChange={(event) => props.handleChange(event)}
-			/>
-			{item}
+      />
+      {item}
     </label>
-	});
+  });
   return <div className={props.className}>
     <p>{props.question.text}</p>
     {list}
